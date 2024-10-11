5aa6b51b72f8e217de4e5d510036eb5615dd66eb
DATDK009 <datntph39527@fpt.edu.vn>
2024-10-12 00:49:24 +0700

    Thay đổi dữ liệu file attribute ,attributeValue ,attributeValueProducts, Product,CategoryProduct , productImag thư mục modelmodel

--- app/Models/AttributeValueProduct.php
+++ app/Models/AttributeValueProduct.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class AttributeValueProduct extends Model
 {
     use HasFactory;
+    protected $table = "attribute_value_products";
+    protected $fillable = ['id', 'product_id', 'attribute_value_id', 'quantity', 'price'];
+    public function product(){
+        $this ->hasMany(Product::class);
+    }
+    public function attributeValue(){
+        $this ->belongsTo(AttributeValue::class,'attribute_value_id',);
+    }
+    public function orderItem(){
+        $this ->hasMany(OrderItem::class);
+    }
+    // public function carts(){
+    //     $this ->belongsTo(Cart::class,'id');
+    // }
+    public function products(){
+        $this ->belongsTo(Product::class,'product_id');
+    }
 }
