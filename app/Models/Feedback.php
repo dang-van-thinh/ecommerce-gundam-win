bc2380c9bfe077ded56204f85fa0d8318cdd2356
kynguyen004 <kynbph39566@fpt.edu.vn>
2024-10-12 11:08:56 +0700

    update file Order, OderItem, Refund, Feedback ben Model

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+    
+    protected $table = "feedback";
+
+    protected $filltable = [
+        'id',
+        'product_id',
+        'user_id',
+        'parent_feedback_id',
+        'rating ',
+        'comment ',
+        'file_path ',
+        'created_at',
+        'updated_at '
+    ];
+    public $timestamps = false;
+
+    public function product(){
+        $this->belongsTo(Product::class,'product_id');
+    }
+
+    public function user(){
+        $this->belongsTo(User::class,'user_id');
+    }
+
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table = "oders";
+
+    protected $filltable = [
+        'id',
+        'address_user_id',
+        'total_amount',
+        'status','payment_method ',
+        'confirmation_status ',
+        'note',
+        'discount_amount',
+        'created_at ',
+        'updated_at '
+    ];
+    public $timestamps = false;
+    public function orderItems(){
+        $this->haMany(OrderItem::class);
+    }
+
+    public function refund(){
+        $this->hasOne(Refund::class);
+    }
+
+    public function addressUser(){
+        $this->belongsTo(AddressUser::class, 'address_user_id');
+    }
 }
