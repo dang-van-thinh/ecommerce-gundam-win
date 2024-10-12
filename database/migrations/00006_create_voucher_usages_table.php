c5f80d4804ad9c7c4ae830bd6bc830f8876926bc
QuanNVph3649 <quannvph36493@fpt.edu.vn>
2024-10-12 20:53:10 +0700

    update migrate voucher, voucher_usage

--- database/migrations/00006_create_voucher_usages_table.php
+++ database/migrations/00006_create_voucher_usages_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\User;
-use App\Models\Vourcher;
+use App\Models\Voucher;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vourcher_usages', function (Blueprint $table) {
+        Schema::create('voucher_usages', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained();
-            $table->foreignIdFor(Vourcher::class)->constrained();
+            $table->foreignIdFor(Voucher::class)->constrained();
             $table->string('vourcher_code');
             $table->dateTime('start_date');
             $table->dateTime('end_date');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vourcher_usages');
+        Schema::dropIfExists('voucher_usages');
     }
 };
