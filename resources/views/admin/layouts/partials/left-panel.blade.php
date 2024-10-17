86748c6e91cbe810976df6a06cf51d2348bcbdaa
kynguyen004 <kynbph39566@fpt.edu.vn>
2024-10-18 00:40:03 +0700

    Crud bảng Attributes và AttributeValue

--- resources/views/admin/layouts/partials/left-panel.blade.php
+++ resources/views/admin/layouts/partials/left-panel.blade.php
@@ -14,6 +14,15 @@
                         <li><i class="fa fa-list"></i><a href="">Mẫu</a></li>
                     </ul>
                 </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-book"></i>QL Thuộc tính
+                    </a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-list"></i><a href="{{ route('attributes.index') }}">Thuộc tính</a></li>
+                        <li><i class="fa fa-list"></i><a href="{{ route('attributeValues.index') }}">Giá trị thuộc tính</a></li>
+                    </ul>
+                </li>
 
             </ul>
         </div>
