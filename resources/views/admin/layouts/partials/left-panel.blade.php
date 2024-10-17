a253d32d4e5a94ad1803dc48476e9d64d71cc471
DATDK009 <datntph39527@fpt.edu.vn>
2024-10-17 21:33:18 +0700

    Thực hiện chức năng thêm sửa xóa bảng categoy product và bảng categori article

--- resources/views/admin/layouts/partials/left-panel.blade.php
+++ resources/views/admin/layouts/partials/left-panel.blade.php
@@ -14,6 +14,14 @@
                         <li><i class="fa fa-list"></i><a href="">Mẫu</a></li>
                     </ul>
                 </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-book"></i>QL Danh mục</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-list"></i><a href="{{route('categoryproduct.index')}}">Danh mục sản phẩm</a></li>
+                        <li><i class="fa fa-list"></i><a href="{{route('categoryarticle.index')}}">Danh mục bài viết</a></li>
+                    </ul>
+                </li>
 
             </ul>
         </div>
