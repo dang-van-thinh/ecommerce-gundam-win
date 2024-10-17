a498008137c381d6f2fabea7a998fcedc8c51aea
kynguyen004 <kynbph39566@fpt.edu.vn>
2024-10-18 00:40:03 +0700

    Crud bảng Attributes và AttributeValue

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\attributes\AttributeController;
+use App\Http\Controllers\attributes\AttributeValueController;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 // Route::get("/home", [Controller::class, 'notification'])->name("home");
 Route::get("/test", [Controller::class, 'test'])->name("test");
+Route::resource('attributes', AttributeController::class);
+Route::resource('attributeValues', AttributeValueController::class);
