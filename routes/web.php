abc04bf6e10a1b5192be38aeb3517bbbb6d52dae
Nghiepnvph33894 <150454030+Nghiepnvph33894@users.noreply.github.com>
2024-10-23 22:40:41 +0700

    add: crud sản phẩm

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\AttributeValueController;
 use App\Http\Controllers\Admin\CategoryArticleController;
 use App\Http\Controllers\Admin\CategoryProductController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VocuherController;
@@ -56,8 +57,7 @@ Route::resource('roles', RoleController::class);
 Route::resource('users', UserController::class);
 Route::resource('voucher', VocuherController::class);
 Route::resource('refund', RefundController::class);
-
-
+Route::resource('products',AdminProductController::class);
 
 // client
 Route::get('/', [HomeController::class, 'index'])->name('home');
