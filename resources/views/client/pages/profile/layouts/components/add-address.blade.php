bc74463346016108d13e015c93082b7938021bbf
Dang Van Thinh <dangvanthinh372004@gmail.com>
2024-10-28 23:56:08 +0700

    update layout component profile user address

---
+++ resources/views/client/pages/profile/layouts/components/add-address.blade.php
@@ -0,0 +1,47 @@
+<div class="reviews-modal modal theme-modal fade" id="add-address" tabindex="-1" role="dialog" aria-modal="true">
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4>Thêm mới địa chỉ giao hàng</h4>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body pt-0">
+                <div class="row g-3">
+                    <div class="col-6">
+                        <div class="from-group">
+                            <label class="form-label">First Name</label><input class="form-control" type="text"
+                                name="review[author]" placeholder="Enter your name." />
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="from-group">
+                            <label class="form-label">Last Name</label><input class="form-control" type="text"
+                                name="review[author]" placeholder="Enter your name." />
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="from-group">
+                            <label class="form-label">Email address</label><input class="form-control" type="email"
+                                placeholder="john.smith@example.com" />
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="from-group">
+                            <label class="form-label">Phone</label><input class="form-control" type="number"
+                                name="review[author]" placeholder="Enter your Number." />
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="from-group">
+                            <label class="form-label">Address</label>
+                            <textarea class="form-control" cols="30" rows="5" placeholder="Write your Address..."></textarea>
+                        </div>
+                    </div>
+                    <button class="btn btn-submit" type="submit" data-bs-dismiss="modal" aria-label="Close">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
