4ba593f01a0a8f81de8b0a4d8d99547adc0b2e36
kynguyen004 <kynguyen200004@gmail.com>
2024-10-29 16:18:16 +0700

    làm bình luận phân cấp và phải đăng nhập mới được bình luận

--- app/Http/Controllers/Client/BlogController.php
+++ app/Http/Controllers/Client/BlogController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\CategoryArticle;
+use App\Models\CommentPost;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function index($id)
+    public function index()
     {
-        $articles = Article::findOrFail($id);
+        $articles = Article::orderBy('id', 'desc')->paginate(10); // Lấy danh sách bài viết
         $latestPosts = Article::orderBy('id', 'desc')->take(4)->get();
-        $categories = CategoryArticle::withCount('articles')->get();
-
+        $categories = CategoryArticle::withCount('articles')->get();   
         return view('client.pages.blog.index', compact('articles', 'latestPosts', 'categories'));
-    }
+    }    
+
     public function articlesByCategory($id)
     {
         // Lấy thể loại theo id
@@ -34,4 +35,44 @@ class BlogController extends Controller
         // Trả về view với các dữ liệu tương ứng
         return view('client.pages.collection-blog.index', compact('articles', 'latestPosts', 'categories', 'category'));
     }
+
+    public function show($id)
+    {
+        // Tìm bài viết theo ID
+        $article = Article::findOrFail($id);
+        
+        // Lấy các bình luận cho bài viết, cùng với thông tin người dùng
+        $comments = CommentPost::with(['user', 'replies.user'])
+        ->where('article_id', $article->id)
+        ->whereNull('parent_comment_id') // Chỉ lấy các comment chính (không có comment cha)
+        ->get();
+
+    
+        // Lấy các bài viết mới nhất
+        $latestPosts = Article::orderBy('id', 'desc')->take(4)->get();
+    
+        // Lấy danh sách các thể loại với số lượng bài viết
+        $categories = CategoryArticle::withCount('articles')->get();
+    
+        // Truyền các biến sang view
+        return view('client.pages.blog.index', compact('article', 'comments', 'latestPosts', 'categories'));
+    }
+    
+    public function storeComment(Request $request, $articleId)
+    {
+        $request->validate([
+            'comment' => 'required|string',
+            'parent_comment_id' => 'nullable|integer', // Nếu có ID của bình luận cha
+        ]);
+    
+        CommentPost::create([
+            'article_id' => $articleId,
+            'user_id' => auth()->id(),
+            'parent_comment_id' => $request->input('parent_comment_id'), // Nếu có
+            'comment' => $request->input('comment'),
+        ]);
+    
+        return redirect()->route('blog.show', $articleId);
+    }
+    
 }
