d358eba0ce9a4436c049b4a5ab6aff3275a0b7e2
Nghiepnvph33894 <150454030+Nghiepnvph33894@users.noreply.github.com>
2024-10-30 22:44:28 +0700

    add: Đặt hàng thành công

--- resources/views/client/pages/order-success/index.blade.php
+++ resources/views/client/pages/order-success/index.blade.php
@@ -8,8 +8,8 @@
             <div class="row">
                 <div class="col-12 px-0">
                     <div class="order-box-1"><img src="/template/client/assets/images/gif/success.gif" alt="">
-                        <h4>Order Success</h4>
-                        <p>Payment Is Successfully Processsed And Your Order Is On The Way</p>
+                        <h4>Đặt hàng thành công</h4>
+                        <p>Thanh toán đã được xử lý thành công và đơn hàng của bạn đang trên đường</p>
                     </div>
                 </div>
             </div>
@@ -20,77 +20,55 @@
             <div class="row gy-4">
                 <div class="col-xl-8">
                     <div class="order-items sticky">
-                        <h4>Order Information </h4>
-                        <p>Order invoice has been send to your registered email account. double check your order details
+                        <h4>Thông tin đặt hàng</h4>
+                        <p>
+                            Hóa đơn đặt hàng đã được gửi đến tài khoản email đã đăng ký của bạn. Kiểm tra lại thông tin chi
+                            tiết đơn hàng của bạn
                         </p>
                         <div class="order-table">
                             <div class="table-responsive theme-scrollbar">
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th>Product </th>
-                                            <th>Price </th>
-                                            <th>Quantity</th>
-                                            <th>Total</th>
+                                            <th>Sản phẩm </th>
+                                            <th>Giá </th>
+                                            <th>Số lượng</th>
+                                            <th>Tổng</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>
-                                                <div class="cart-box"> <a href="product.html"> <img
-                                                            src="/template/client/assets/images/cart/category/1.jpg"
-                                                            alt=""></a>
-                                                    <div> <a href="product.html">
-                                                            <h5>Concrete Jungle Pack</h5>
+                                        @foreach ($data->orderItems as $item)
+                                            <tr>
+                                                <td>
+                                                    <div class="cart-box">
+                                                        <a href="product.html">
+                                                            <img src="/template/client/assets/images/cart/category/1.jpg"
+                                                                alt="">
                                                         </a>
-                                                        <p>Sold By: <span>Roger Group </span></p>
-                                                        <p>Size: <span>Small</span></p>
+                                                        <div>
+                                                            <a href="product.html">
+                                                                <h5>{{ $item->product_name }}</h5>
+                                                            </a>
+                                                            @foreach ($item->productVariant->attributeValues as $attributeValue)
+                                                                <p>{{ $attributeValue->attribute->name }}:
+                                                                    <span>{{ $attributeValue->name }}</span>
+                                                                </p>
+                                                            @endforeach
+
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </td>
-                                            <td>$20.00</td>
-                                            <td>01</td>
-                                            <td>$195.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="cart-box"> <a href="product.html"> <img
-                                                            src="/template/client/assets/images/cart/category/2.jpg"
-                                                            alt=""></a>
-                                                    <div> <a href="product.html">
-                                                            <h5>Mini dress with ruffled straps</h5>
-                                                        </a>
-                                                        <p>Sold By: <span>luisa Shop</span></p>
-                                                        <p>Size: <span>Medium </span></p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>$20.00</td>
-                                            <td>02</td>
-                                            <td>$150.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="cart-box"> <a href="product.html"> <img
-                                                            src="/template/client/assets/images/cart/category/3.jpg"
-                                                            alt=""></a>
-                                                    <div> <a href="product.html">
-                                                            <h5>Long Sleeve Asymmetric</h5>
-                                                        </a>
-                                                        <p>Sold By: <span>Brown Shop</span></p>
-                                                        <p>Size: <span>Large </span></p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>$25.00</td>
-                                            <td>05</td>
-                                            <td>$120.00</td>
-                                        </tr>
+                                                </td>
+                                                <td>{{ number_format($item->product_price) }} VND</td>
+                                                <td>{{ $item->quantity }}</td>
+                                                <td>{{ number_format($item->total_price) }} VND</td>
+                                            </tr>
+                                        @endforeach
+
                                         <tr>
                                             <td> </td>
                                             <td></td>
-                                            <td class="total fw-bold">Total : </td>
-                                            <td class="total fw-bold">$465.00</td>
+                                            <td class="total fw-bold">Tổng : </td>
+                                            <td class="total fw-bold">{{ number_format($data->total_amount) }} VND</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -102,12 +80,12 @@
                     <div class="summery-box">
                         <div class="sidebar-title">
                             <div class="loader-line"></div>
-                            <h4>Order Details </h4>
+                            <h4>Chi tiết đơn hàng</h4>
                         </div>
                         <div class="summery-content">
                             <ul>
                                 <li>
-                                    <p class="fw-semibold">Product total (3)</p>
+                                    <p class="fw-semibold">Tổng sản phẩm ({{ count($data->orderItems) }})</p>
                                     <h6>$230.00</h6>
                                 </li>
                                 <li>
@@ -129,12 +107,12 @@
                                 </li>
                             </ul>
                             <div class="d-flex align-items-center justify-content-between">
-                                <h6>Total (USD)</h6>
-                                <h5>$265.00</h5>
+                                <h6>Tổng cộng</h6>
+                                <h5>{{ number_format($data->discount_amount) }} VND</h5>
                             </div>
                             <div class="note-box">
-                                <p>I'm hoping the store can work with me to get it delivered as soon as possible because
-                                    I really need it to gift to my friend for her party next week.Many thanks for it.
+                                <p>Tôi hy vọng cửa hàng có thể làm việc với tôi để giao hàng sớm nhất có thể vì tôi thực sự
+                                    cần nó để tặng cho bạn tôi vào bữa tiệc tuần tới. Cảm ơn bạn rất nhiều.
                                 </p>
                             </div>
                         </div>
@@ -142,7 +120,7 @@
                     <div class="summery-footer">
                         <div class="sidebar-title">
                             <div class="loader-line"></div>
-                            <h4>Shipping Address</h4>
+                            <h4>Địa chỉ giao hàng</h4>
                         </div>
                         <ul>
                             <li>
