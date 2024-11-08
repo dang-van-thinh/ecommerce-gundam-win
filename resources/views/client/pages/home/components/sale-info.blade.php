1e28b71df539b4c21ca63c97a4d561d38e51849f
Dang Van Thinh <dangvanthinh372004@gmail.com>
2024-11-08 22:56:24 +0700

    update layout client product wishlist

--- resources/views/client/pages/home/components/sale-info.blade.php
+++ resources/views/client/pages/home/components/sale-info.blade.php
@@ -3,53 +3,52 @@
         <div class="col-xxl-5 col-lg-8 offer-box-1">
             <div class="row gy-4 ratio_45">
                 @foreach ($contentLeftTopBanners as $banners)
-                <div class="col-12">
-                    <div class="collection-banner p-left"> 
-                        
-                        <img class="bg-img"
-                            src="{{ asset('storage/' . $banners->image_url) }}" alt="" />
-                        <div class="contain-banners">
-                            <div>
-                                <h4>Up to 60% OFF</h4>
-                                <h3>{{$banners->title}}</h3>
-                                <div class="link-hover-anim underline"><a
-                                        class="btn btn_underline link-strong link-strong-unhovered"
-                                        href="{{$banners->link}}">Shop Collection<svg>
-                                            <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
-                                            </use>
-                                        </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
-                                        href="{{$banners->link}}">Shop Collection<svg>
-                                            <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
-                                            </use>
-                                        </svg></a></div>
+                    <div class="col-12">
+                        <div class="collection-banner p-left">
+
+                            <img class="bg-img" src="{{ asset('storage/' . $banners->image_url) }}" alt="" />
+                            <div class="contain-banners">
+                                <div>
+                                    <h4>Up to 60% OFF</h4>
+                                    <h3>{{ $banners->title }}</h3>
+                                    <div class="link-hover-anim underline"><a
+                                            class="btn btn_underline link-strong link-strong-unhovered"
+                                            href="{{ $banners->link }}">Shop Collection<svg>
+                                                <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
+                                                </use>
+                                            </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
+                                            href="{{ $banners->link }}">Shop Collection<svg>
+                                                <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
+                                                </use>
+                                            </svg></a></div>
+                                </div>
                             </div>
+
                         </div>
-                        
-                    </div>    
-                </div>
+                    </div>
                 @endforeach
-                @foreach ($contentLeftBelowBanners as $banners)             
-                <div class="col-12">
-                    <div class="collection-banners p-right"><img class="bg-img"
-                            src="{{ asset('storage/' . $banners->image_url) }}" alt="" />
-                        <div class="contain-banners">
-                            <div>
-                                <h4>Up to 60% OFF</h4>
-                                <h3>{{$banners->title}}</h3>
-                                <div class="link-hover-anim underline"><a
-                                        class="btn btn_underline link-strong link-strong-unhovered"
-                                        href="{{$banners->link}}">Shop Collection<svg>
-                                            <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
-                                            </use>
-                                        </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
-                                        href="{{$banners->link}}">Shop Collection<svg>
-                                            <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
-                                            </use>
-                                        </svg></a></div>
+                @foreach ($contentLeftBelowBanners as $banners)
+                    <div class="col-12">
+                        <div class="collection-banners p-right"><img class="bg-img"
+                                src="{{ asset('storage/' . $banners->image_url) }}" alt="" />
+                            <div class="contain-banners">
+                                <div>
+                                    <h4>Up to 60% OFF</h4>
+                                    <h3>{{ $banners->title }}</h3>
+                                    <div class="link-hover-anim underline"><a
+                                            class="btn btn_underline link-strong link-strong-unhovered"
+                                            href="{{ $banners->link }}">Shop Collection<svg>
+                                                <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
+                                                </use>
+                                            </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
+                                            href="{{ $banners->link }}">Shop Collection<svg>
+                                                <use href="/template/client/assets/svg/icon-sprite.svg#arrow">
+                                                </use>
+                                            </svg></a></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </div>
@@ -61,41 +60,26 @@
                         @foreach ($products as $product)
                             <div class="swiper-slide product-box-3">
                                 <div class="img-wrapper">
-                                    <div class="label-block"><span class="lable-1">NEW</span><a
+                                    <div class="label-block">
+                                        <span class="lable-1">NEW</span><a
                                             class="label-2 wishlist-icon" href="javascript:void(0)" tabindex="0"><i
                                                 class="iconsax" data-icon="heart" aria-hidden="true"
                                                 data-bs-toggle="tooltip" data-bs-title="Add to Wishlist"></i></a>
                                     </div>
                                     <div class="product-image ratio_apos">
-                                    <a class="pro-first" href="{{ route('product', $product->id) }}">
-                                        <img class="bg-img" src="{{ '/storage/' . $product->image}}" alt="product" />
-                                    </a>
-                                    @php
-                                    $firstImage = $product->productImages->first();
-                                    @endphp
-                                    <a class="pro-sec" href="{{ route('product', $product->id) }}">
-                                        <img class="bg-img" src="{{'/storage/' . $firstImage->image_url}}"
-                                            alt="product" />
-                                    </a>
-                                </div>
-                                    <div class="cart-info-icon">
-                                        <a href="#" data-bs-toggle="modal" data-bs-target="#addtocart"
-                                            tabindex="0">
-                                            <i class="iconsax" data-icon="basket-2" aria-hidden="true"
-                                                data-bs-toggle="tooltip" data-bs-title="Add to card">
-                                            </i>
+                                        <a class="pro-first" href="{{ route('product', $product->id) }}">
+                                            <img class="bg-img" src="{{ '/storage/' . $product->image }}"
+                                                alt="product" />
                                         </a>
-                                        {{-- <a href="compare.html" tabindex="0">
-                                        <i class="iconsax" data-icon="arrow-up-down" aria-hidden="true"
-                                            data-bs-toggle="tooltip" data-bs-title="Compare">
-                                        </i>
-                                    </a> --}}
-                                        <a href="#" data-bs-toggle="modal" data-bs-target="#quick-view"
-                                            tabindex="0"><i class="iconsax" data-icon="eye" aria-hidden="true"
-                                                data-bs-toggle="tooltip" data-bs-title="Quick View">
-                                            </i>
+                                        @php
+                                            $firstImage = $product->productImages->first();
+                                        @endphp
+                                        <a class="pro-sec" href="{{ route('product', $product->id) }}">
+                                            <img class="bg-img" src="{{ '/storage/' . $firstImage->image_url }}"
+                                                alt="product" />
                                         </a>
                                     </div>
+
                                 </div>
                                 <div class="product-detail">
                                     <ul class="rating">
@@ -112,7 +96,8 @@
                                         @if ($product->productVariants->count() === 1)
                                             {{ number_format($product->productVariants->first()->price, 0, ',', '.') }}₫
                                         @else
-                                            {{ number_format($product->productVariants->min('price'), 0, ',', '.') }}₫ -
+                                            {{ number_format($product->productVariants->min('price'), 0, ',', '.') }}₫
+                                            -
                                             {{ number_format($product->productVariants->max('price'), 0, ',', '.') }}₫
                                         @endif
                                     </p>
@@ -190,16 +175,16 @@
             </div>
         </div>
         @foreach ($contentRightBanners as $banner)
-        <div class="col-4 d-none d-xxl-block">
-            <div class="offer-banners-3 ratio1_3">
-                 <a href="{{$banner->link}}"> <img class="bg-img"
-                        src="{{ asset('storage/' . $banner->image_url) }}" alt="" />
-                    <div> <img src="/template/client/assets/images/banner/2.png" alt="" />
-                        <h6>{{$banner->title}}</h6>
-                    </div>
-                </a>
+            <div class="col-4 d-none d-xxl-block">
+                <div class="offer-banners-3 ratio1_3">
+                    <a href="{{ $banner->link }}"> <img class="bg-img"
+                            src="{{ asset('storage/' . $banner->image_url) }}" alt="" />
+                        <div> <img src="/template/client/assets/images/banner/2.png" alt="" />
+                            <h6>{{ $banner->title }}</h6>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
 </div>
