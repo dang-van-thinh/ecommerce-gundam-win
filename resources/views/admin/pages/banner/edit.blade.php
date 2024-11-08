8c7624b2bb4f4029dd2387b59587333046e08e3d
thutrang1502 <117855800+thutrang1502@users.noreply.github.com>
2024-11-08 21:10:06 +0700

    Hiển thị banner client

--- resources/views/admin/pages/banner/edit.blade.php
+++ resources/views/admin/pages/banner/edit.blade.php
@@ -30,8 +30,13 @@
                     <label for="image_type" class="form-label">Chọn vị trí hình ảnh:</label>
                     <select id="image_type" name="image_type" class="form-control">
                         <option value="header" {{ $banners->image_type == 'header' ? 'selected' : '' }}>Header</option>
-                        <option value="content" {{ $banners->image_type == 'content' ? 'selected' : '' }}>Content</option>
-                    </select>
+                        <option value="CONTENT-LEFT-TOP" {{ $banners->image_type == 'CONTENT-LEFT-TOP' ? 'selected' : '' }}>Content-left-top</option>
+                        <option value="CONTENT-LEFT-BELOW" {{ $banners->image_type == 'CONTENT-LEFT-BELOW' ? 'selected' : '' }}>Content-left-right</option>
+                        <option value="CONTENT-RIGHT" {{ $banners->image_type == 'CONTENT-RIGHT' ? 'selected' : '' }}>Content-right</option>
+                        <option value="SUBSCRIBE-NOW-EMAIL" {{ $banners->image_type == 'SUBSCRIBE-NOW-EMAIL' ? 'selected' : '' }}>subscribe-now-email</option>
+                        <option value="BANNER-LEFT" {{ $banners->image_type == 'BANNER-LEFT' ? 'selected' : '' }}>banner-left</option>
+                        <option value="BANNER-RIGHT" {{ $banners->image_type == 'BANNER-RIGHT' ? 'selected' : '' }}>banner-right</option>
+                        </select>
                     @error('image_type')
                     <span class="text-danger">{{ $message }}</span>
                     @enderror
