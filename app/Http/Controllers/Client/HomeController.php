d7a73f23c05bb7d783da70fbd6ffe41d4f85122c
thutrang1502 <117855800+thutrang1502@users.noreply.github.com>
2024-11-08 21:10:06 +0700

    Hiển thị banner client

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use App\Models\Product;
 
 class HomeController extends Controller
@@ -13,7 +14,27 @@ class HomeController extends Controller
             ->orderBy('love', 'desc') // Sắp xếp theo trường love
             ->take(5) // Lấy 5 sản phẩm có giá trị love cao nhất
             ->get(); // Trả về danh sách sản phẩm
-        return view('client.pages.home.index', compact('products'));
+
+         // Lấy theo phân loại banner
+         $headerBanners = Banner::where('image_type','HEADER')->get();
+        $contentLeftTopBanners = Banner::where('image_type','CONTENT-LEFT-TOP')->get();
+        $contentLeftBelowBanners = Banner::where('image_type','CONTENT-LEFT-BELOW')->get();
+        $contentRightBanners = Banner::where('image_type','CONTENT-RIGHT')->get();
+        $subscribeNowEmailBanners = Banner::where('image_type','SUBSCRIBE-NOW-EMAIL')->get();
+        $leftBanners = Banner::where('image_type','BANNER-LEFT')->get();
+        $rightBanners = Banner::where('image_type','BANNER-RIGHT')->get();
+         
+        return view('client.pages.home.index', compact(
+            'products', 
+            'headerBanners', 
+            'contentLeftTopBanners', 
+            'contentLeftBelowBanners', 
+            'contentRightBanners', 
+            'subscribeNowEmailBanners', 
+            'leftBanners', 
+            'rightBanners'
+        ));
+        
     }
     public function loadAllCollection() {
     }
