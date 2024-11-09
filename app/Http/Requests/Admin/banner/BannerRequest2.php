76b75b6eeab7a3dbb9db87545f2324406dfb942b
thutrang1502 <117855800+thutrang1502@users.noreply.github.com>
2024-11-09 22:44:13 +0700

    update banner client

--- app/Http/Requests/Admin/banner/BannerRequest2.php
+++ app/Http/Requests/Admin/banner/BannerRequest2.php
@@ -23,14 +23,41 @@ class BannerRequest2 extends FormRequest
             'image_type' => 'required|in:HEADER,CONTENT-LEFT-TOP,CONTENT-LEFT-BELOW,CONTENT-RIGHT,SUBSCRIBE-NOW-EMAIL,BANNER-LEFT,BANNER-RIGHT',
             'link' => 'required|nullable|url',
         ];
-    }
+        // Kiểm tra image_type và thêm quy tắc kích thước ảnh
+        switch ($this->input('image_type')) {
+            case 'HEADER':
+                $rules['image'] .= '|dimensions:min_width=930,min_height=380'; // Header yêu cầu kích thước tối thiểu 930x380
+                break;
+            case 'CONTENT-LEFT-TOP':
+                $rules['image'] .= '|dimensions:min_width=650,min_height=300'; // CONTENT-LEFT-TOP yêu cầu 650x300
+                break;
+            case 'CONTENT-LEFT-BELOW':
+                $rules['image'] .= '|dimensions:min_width=650,min_height=300'; // CONTENT-LEFT-BELOW yêu cầu 650x300
+                break;
+            case 'CONTENT-RIGHT':
+                $rules['image'] .= '|dimensions:min_width=500,min_height=550'; // CONTENT-RIGHT yêu cầu 500x550
+                break;
+            case 'SUBSCRIBE-NOW-EMAIL':
+                $rules['image'] .= '|dimensions:min_width=1100,min_height=700'; // SUBSCRIBE-NOW-EMAIL yêu cầu 1100x700
+                break;
+            case 'BANNER-LEFT':
+                $rules['image'] .= '|dimensions:min_width=700,min_height=700'; // BANNER-LEFT yêu cầu 700x700
+                break;
+            case 'BANNER-RIGHT':
+                $rules['image'] .= '|dimensions:min_width=700,min_height=500'; // BANNER-RIGHT yêu cầu 700x500
+                break;
+        }
 
-    // Custom error messages
+        return $rules;
+    }
+    
     public function messages()
     {
         return [
             'title.required' => 'Không được để trống',
             'image.required' => 'Không được để trống hình ảnh',
+            'image.image' => 'Định dạng hình ảnh không hợp lệ',
+            'image.dimensions' => 'Kích thước hình ảnh không hợp lệ cho loại hình ảnh đã chọn',
             'link.required' => 'Không được để trống',
         ];
     }
