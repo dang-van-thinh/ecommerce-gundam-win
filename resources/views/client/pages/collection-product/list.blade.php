04227a73795a8680654040ca21bde7281f579cdf
Hoaky2004 <nguyenhoaky2004@gmail.com>
2024-11-16 16:28:17 +0700

    Thực hiện làm trang chủ và hiển thị sao đánh giá ở phần sản phẩm

--- resources/views/client/pages/collection-product/list.blade.php
+++ resources/views/client/pages/collection-product/list.blade.php
@@ -14,34 +14,39 @@
                         </div>
                         <div class="product-image ratio_apos">
                             <a class="pro-first" href="{{ route('product', $product->id) }}">
-                                <img class="bg-img" src="{{ '/storage/' . $product->image }}" alt="product" style="width: 100%; height: 300px; object-fit: cover;" />
+                                <img class="bg-img" src="{{ '/storage/' . $product->image }}" alt="product"
+                                    style="width: 100%; height: 300px; object-fit: cover;" />
                             </a>
                             @php
                                 $firstImage = $product->productImages->first();
                             @endphp
                             <a class="pro-sec" href="{{ route('product', $product->id) }}">
-                                <img class="bg-img" src="{{ '/storage/' . $firstImage->image_url }}" alt="product" style="width: 100%; height: 300px; object-fit: cover;" />
+                                <img class="bg-img" src="{{ '/storage/' . $firstImage->image_url }}" alt="product"
+                                    style="width: 100%; height: 300px; object-fit: cover;" />
                             </a>
                         </div>
                     </div>
                     <div class="product-detail">
                         <ul class="rating">
-                            <li><i class="fa-solid fa-star"></i></li>
-                            <li><i class="fa-solid fa-star"></i></li>
-                            <li><i class="fa-solid fa-star"></i></li>
-                            <li><i class="fa-solid fa-star-half-stroke"></i></li>
-                            <li><i class="fa-regular fa-star"></i></li>
-                            <li>4.3</li>
+                            @php
+                                $rating = $product->average_rating ? $product->average_rating : 0; // Lấy rating từ feedback
+                            @endphp
+                            @for ($i = 1; $i <= 5; $i++)
+                                <li>
+                                    <i class="fa-solid fa-star"
+                                        style="color: {{ $i <= $rating ? '#f39c12' : '#000' }};"></i>
+                                </li>
+                            @endfor
                         </ul>
                         <a href="{{ route('product', $product->id) }}">
                             <h6>{{ $product->name }}</h6>
                         </a>
                         <p>
                             @if ($product->productVariants->count() === 1)
-                                {{ number_format($product->productVariants->first()->price, 0, ',', '.') }}₫
+                                {{ number_format($product->productVariants->first()->price, 0, ',', '.') }} VND
                             @else
-                                {{ number_format($product->productVariants->min('price'), 0, ',', '.') }}₫-
-                                {{ number_format($product->productVariants->max('price'), 0, ',', '.') }}₫
+                                {{ number_format($product->productVariants->min('price'), 0, ',', '.') }} -
+                                {{ number_format($product->productVariants->max('price'), 0, ',', '.') }} VND
                             @endif
                         </p>
                     </div>
@@ -55,53 +60,52 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-               $(document).ready(function () {
-            $(document).on('click', '.wishlist-icon', function (e) {
+        $(document).ready(function() {
+        $(document).on('click', '.wishlist-icon', function(e) {
                 e.preventDefault();
                 var productId = $(this).data('id');
                 var icon = $(this).find('i');
                 @auth
-                    $.ajax({
-                        url: '{{ route("toggle.favorite") }}',
-                        method: 'POST',
-                        data: {
-                            userId: {{ Auth::id() }},
-                            product_id: productId
-                        },
-                        success: function (response) {
-                            console.log("Love status:", response);
-                            $('#love').text(response.love);
+                $.ajax({
+                    url: '{{ route('toggle.favorite') }}',
+                    method: 'POST',
+                    data: {
+                        userId: {{ Auth::id() }},
+                        product_id: productId
+                    },
+                    success: function(response) {
+                        console.log("Love status:", response);
+                        $('#love').text(response.love);
 
-                            // Cập nhật trạng thái hiển thị icon và văn bản
-                            if (response.status === 'added') {
-                                icon.eq(0).hide();  // Ẩn trái tim chưa yêu thích
-                                icon.eq(1).show();  // Hiển thị trái tim đã yêu thích
-                            } else {
-                                icon.eq(1).hide();  // Ẩn trái tim đã yêu thích
-                                icon.eq(0).show();  // Hiển thị trái tim chưa yêu thích
-                            }
-                        },
-                        error: function (xhr, status, error) {
-                            Swal.fire("Có lỗi xảy ra!", "", "error");
-                            console.error(error);
+                        // Cập nhật trạng thái hiển thị icon và văn bản
+                        if (response.status === 'added') {
+                            icon.eq(0).hide(); // Ẩn trái tim chưa yêu thích
+                            icon.eq(1).show(); // Hiển thị trái tim đã yêu thích
+                        } else {
+                            icon.eq(1).hide(); // Ẩn trái tim đã yêu thích
+                            icon.eq(0).show(); // Hiển thị trái tim chưa yêu thích
                         }
-                    });
-                @endauth
+                    },
+                    error: function(xhr, status, error) {
+                        Swal.fire("Có lỗi xảy ra!", "", "error");
+                        console.error(error);
+                    }
+                });
+            @endauth
 
-                @guest
-                    Swal.fire({
-                        title: "Bạn cần đăng nhập để thực hiện thao tác này!",
-                        icon: "warning",
-                        confirmButtonText: "Đăng nhập",
-                        showCancelButton: true,
-                        cancelButtonText: "Hủy"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            window.location.href = "{{ route('auth.login-view') }}";
-                        }
-                    });
-                @endguest
+            @guest Swal.fire({
+                title: "Bạn cần đăng nhập để thực hiện thao tác này!",
+                icon: "warning",
+                confirmButtonText: "Đăng nhập",
+                showCancelButton: true,
+                cancelButtonText: "Hủy"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "{{ route('auth.login-view') }}";
+                }
             });
+        @endguest
+        });
         });
     </script>
 @endpush
--- resources/views/client/pages/home/components/brand.blade.php
+++ resources/views/client/pages/home/components/brand.blade.php
@@ -3,88 +3,55 @@
         <div class="col-12">
             <div class="d-sm-flex d-block justify-content-between align-items-center">
                 <div class="title-1">
-                    <p>Brand collection<span></span></p>
-                    <h3>Flash Sale Product</h3>
+                    <p>Bộ sưu tập thương hiệu<span></span></p>
+                    <h3>sản phẩm yêu thích</h3>
                 </div>
-                <div class="link-hover-anim underline"><a
-                        class="btn btn_underline link-strong link-strong-unhovered"
-                        href="collection-left-sidebar.html">See All Product<svg>
+                <div class="link-hover-anim underline"><a class="btn btn_underline link-strong link-strong-unhovered"
+                        href="{{ route('collection-product') }}">Xem tất cả sản phẩm<svg>
                             <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                         </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
-                        href="collection-left-sidebar.html">See All Product<svg>
+                        href="{{ route('collection-product') }}">Xem tất cả sản phẩm<svg>
                             <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                         </svg></a></div>
             </div>
         </div>
         <div class="col-xxl-3 col-lg-6 col-12 order-xxl-1 order-2">
             <div class="row gy-4">
-                <div class="col-lg-12 col-md-6 col-12">
-                    <div class="flash-content"><img class="img-fluid"
-                            src="/template/client/assets/images/banner/6.jpg" alt="" />
-                        <div>
-                            <ul>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li>4.3</li>
-                            </ul><a href="product.html">
-                                <h6>Greciilooks Women's Stylish Top </h6>
+                @foreach ($products->take(2) as $product)
+                    <div class="col-xxl-12 col-md-6 col-12">
+                        <div class="flash-content">
+                            <a class="pro-first" href="{{ route('product', $product->id) }}">
+                                <img class="img-fluid" src="{{ '/storage/' . $product->image }}" alt="product"
+                                    style="width: 150px; height: 190px; object-fit: cover;" />
                             </a>
-                            <h6>$100.00</h6>
+                            <div>
+                                <ul class="rating">
+                                    @php
+                                        $rating = $product->average_rating ? $product->average_rating : 0; // Lấy rating từ feedback
+                                    @endphp
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        <li>
+                                            <i class="fa-solid fa-star"
+                                                style="color: {{ $i <= $rating ? '#f39c12' : '#000' }};"></i>
+                                        </li>
+                                    @endfor
+                                </ul>
+                                <a href="{{ route('product', $product->id) }}">
+                                    <h6>{{ $product->name }}</h6>
+                                </a>
+                                <p>
+                                    @if ($product->productVariants->count() === 1)
+                                        {{ number_format($product->productVariants->first()->price, 0, ',', '.') }} VND
+                                    @else
+                                        {{ number_format($product->productVariants->min('price'), 0, ',', '.') }} -
+                                        {{ number_format($product->productVariants->max('price'), 0, ',', '.') }} VND
+                                    @endif
+                                </p>
+                            </div>
+                            <div class="flash-lable"> <span>-30%</span></div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-12 col-md-6 col-12">
-                    <div class="flash-content"><img class="img-fluid"
-                            src="/template/client/assets/images/banner/7.jpg" alt="" />
-                        <div>
-                            <ul>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li>4.3</li>
-                            </ul><a href="product.html">
-                                <h6>Greciilooks Women's Stylish Top </h6>
-                            </a>
-                            <h6>$100.00<del>$140.00</del></h6>
-                        </div>
-                        <div class="flash-lable"> <span>-20%</span></div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
         <div class="col-xxl-6 col-lg-6 col-12 order-xxl-2 order-1">
@@ -122,10 +89,10 @@
                     </div>
                     <div class="link-hover-anim underline"><a
                             class="btn btn_underline link-strong link-strong-unhovered"
-                            href="collection-left-sidebar.html">Shop Collection<svg>
+                            href="collection-left-sidebar.html">Bộ sưu tập cửa hàng<svg>
                                 <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                             </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
-                            href="collection-left-sidebar.html">Shop Collection<svg>
+                            href="collection-left-sidebar.html">Bộ sưu tập cửa hàng<svg>
                                 <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                             </svg></a></div>
                 </div>
@@ -133,73 +100,41 @@
         </div>
         <div class="col-xxl-3 col-12 order-xxl-3 order-3">
             <div class="row gy-4">
-                <div class="col-xxl-12 col-md-6 col-12">
-                    <div class="flash-content"><img class="img-fluid"
-                            src="/template/client/assets/images/banner/8.jpg" alt="" />
-                        <div>
-                            <ul>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li>4.3</li>
-                            </ul><a href="product.html">
-                                <h6>Greciilooks Women's Stylish Top </h6>
+                @foreach ($products->slice(2, 2) as $product)
+                    <div class="col-xxl-12 col-md-6 col-12">
+                        <div class="flash-content">
+                            <a class="pro-first" href="{{ route('product', $product->id) }}">
+                                <img class="img-fluid" src="{{ '/storage/' . $product->image }}" alt="product"
+                                    style="width: 150px; height: 190px; object-fit: cover;" />
                             </a>
-                            <h6>$100.00<del>$140.00</del></h6>
-                        </div>
-                        <div class="flash-lable"> <span>-30%</span></div>
-                    </div>
-                </div>
-                <div class="col-xxl-12 col-md-6 col-12">
-                    <div class="flash-content"><img class="img-fluid"
-                            src="/template/client/assets/images/banner/9.jpg" alt="" />
-                        <div>
-                            <ul>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li><svg>
-                                        <use href="/template/client/assets/svg/icon-sprite.svg#star">
-                                        </use>
-                                    </svg></li>
-                                <li>4.3</li>
-                            </ul><a href="product.html">
-                                <h6>Greciilooks Women's Stylish Top </h6>
-                            </a>
-                            <h6>$100.00</h6>
+                            <div>
+                                <ul class="rating">
+                                    @php
+                                        $rating = $product->average_rating ? $product->average_rating : 0; // Lấy rating từ feedback
+                                    @endphp
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        <li>
+                                            <i class="fa-solid fa-star"
+                                                style="color: {{ $i <= $rating ? '#f39c12' : '#000' }};"></i>
+                                        </li>
+                                    @endfor
+                                </ul>
+                                <a href="{{ route('product', $product->id) }}">
+                                    <h6>{{ $product->name }}</h6>
+                                </a>
+                                <p>
+                                    @if ($product->productVariants->count() === 1)
+                                        {{ number_format($product->productVariants->first()->price, 0, ',', '.') }} VND
+                                    @else
+                                        {{ number_format($product->productVariants->min('price'), 0, ',', '.') }} -
+                                        {{ number_format($product->productVariants->max('price'), 0, ',', '.') }} VND
+                                    @endif
+                                </p>
+                            </div>
+                            <div class="flash-lable"> <span>-30%</span></div>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
