1b736bb1482bf2475768f62d2c6b4be6fcdd87f5
thutrang1502 <117855800+thutrang1502@users.noreply.github.com>
2024-11-20 00:24:47 +0700

    update banner

--- resources/views/client/pages/home/components/brand.blade.php
+++ resources/views/client/pages/home/components/brand.blade.php
@@ -55,47 +55,55 @@
             </div>
         </div>
         <div class="col-xxl-6 col-lg-6 col-12 order-xxl-2 order-1">
-            <div class="flash-images ratio50_2"><a href="collection-left-sidebar.html"><img class="bg-img"
-                        src="/template/client/assets/images/banner/banner-10.jpg" alt="" /></a>
+            <div class="flash-images ratio50_2">
+                @if ($rightBanners)
+                <a href="{{$rightBanners->link}}"><img class="bg-img"
+                    src="{{ asset('storage/' . $rightBanners->image_url) }}" alt="" /></a>
+               
+                
                 <div class="banner-2">
-                    <h3>40% - 80%Off</h3>
-                    <h5>Men's Special Offer</h5>
+                    <h3>{{$rightBanners->title}}</h3>
+                   
                     <div class="countdown">
                         <ul class="clockdiv8">
                             <li>
                                 <div class="timer">
                                     <div class="days"></div>
-                                </div><span class="title">Days</span>
+                                </div><span class="title">Ngày</span>
                             </li>
                             <li class="dot"> <span> </span><span></span></li>
                             <li>
                                 <div class="timer">
                                     <div class="hours"></div>
-                                </div><span class="title">Hours</span>
+                                </div><span class="title">Giờ</span>
                             </li>
                             <li class="dot"> <span> </span><span></span></li>
                             <li>
                                 <div class="timer">
                                     <div class="minutes"></div>
-                                </div><span class="title">Min</span>
+                                </div><span class="title">Phút</span>
                             </li>
                             <li class="dot"> <span> </span><span></span></li>
                             <li>
                                 <div class="timer">
                                     <div class="seconds"></div>
-                                </div><span class="title">Sec</span>
+                                </div><span class="title">Giây</span>
                             </li>
                         </ul>
                     </div>
                     <div class="link-hover-anim underline"><a
                             class="btn btn_underline link-strong link-strong-unhovered"
-                            href="collection-left-sidebar.html">Bộ sưu tập cửa hàng<svg>
+                            href="collection-left-sidebar.html">Xem thêm<svg>
                                 <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                             </svg></a><a class="btn btn_underline link-strong link-strong-hovered"
-                            href="collection-left-sidebar.html">Bộ sưu tập cửa hàng<svg>
+                            href="collection-left-sidebar.html">Xem thêm<svg>
                                 <use href="/template/client/assets/svg/icon-sprite.svg#arrow"></use>
                             </svg></a></div>
                 </div>
+                @else
+                <a href="collection-left-sidebar.html"><img class="bg-img"
+                    src="/template/client/assets/images/banner/banner-10.jpg" alt="" /></a>
+            @endif
             </div>
         </div>
         <div class="col-xxl-3 col-12 order-xxl-3 order-3">
