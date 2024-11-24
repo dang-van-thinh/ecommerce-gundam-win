573f1b40d96bd416ef0e30739af68aab7a389748
Dang Van Thinh <dangvanthinh372004@gmail.com>
2024-11-25 00:21:00 +0700

    cap nhat lai conflic

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ use App\Http\Controllers\Client\ContactController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\MyVoucherController;
 use App\Http\Controllers\Client\OrderController;
-use App\Http\Controllers\Client\PolicyController;
 use App\Http\Controllers\Client\ProductController;
 use App\Http\Controllers\Client\ProfileController;
 use App\Http\Controllers\Client\SearchController;
